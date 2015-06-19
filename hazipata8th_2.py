b4a5ae3ef01c555bdc30f99e12bc83261de21145
taishi <taishi1991911@gmail.com>
2015-06-19 12:50:00 +0900

    Revert "machine-learning"
    This reverts commit 7064a8a0b2572e428fca83fb24ec7051ab3b699b.

--- hazipata8th_2.py
+++
@@ -1,8 +0,0 @@
-# coding: UTF-8
-import numpy as np
-import matplotlib
-from matplotlib import pyplot as plt
-import scipy
-from sklearn import svm
-
-mu_vec1 = np.array([0,0])
--- multiple3.py
+++
@@ -1,6 +0,0 @@
-#coding: utf8
-for i in range(1, 101):
-	if i%3==0 or "3" in str(i):
-		print "あほ"
-	else:
-		print i
